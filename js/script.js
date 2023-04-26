0ac0057647d1ef9c1e30b8bb09abd9ce7475f777
Raphour <leraphlol@gmail.com>
2023-04-27 00:03:36 +0200

    debut fructueux

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+function changeImage(element) {
+    var image = element.querySelector("img");
+    var nouvelleImage = "../assets/men_button_hover.png";
+    image.src = nouvelleImage;
+    element.style.width = "105%";
+}
+
+
+function resetImage(element) {
+    var image = element.querySelector("img");
+    var nouvelleImage = "../assets/menu_button.png";
+    image.src = nouvelleImage;
+    element.style.width = "100%";
+}
