720d012064d2576b6c53bd7639684703721b3bf4
Raphour <leraphlol@gmail.com>
2023-04-29 00:41:33 +0200

    base fonctionnement systeme dialogue

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 function changeImage(element) {
     var image = element.querySelector("img");
-    var nouvelleImage = "../assets/men_button_hover.png";
+    var nouvelleImage = "../assets/menu_button_hover.png";
     image.src = nouvelleImage;
     element.style.width = "105%";
 }
--- js/scriptDialogue.js
+++ js/scriptDialogue.js
@@ -1,37 +1,68 @@
 // Tableau contenant les dialogues
 var dialogues = [
-    "Premier dialogue",
-    "Deuxième dialogue",
-    "Troisième dialogue"
-  ];
-  
-  // Récupération des éléments du DOM
-  var texteDialogue = document.getElementById("texte-dialogue");
-  var boutonPrecedent = document.getElementById("bouton-precedent");
-  var boutonSuivant = document.getElementById("bouton-suivant");
-  
+  "Premier dialogue",
+  "Deuxième dialogue",
+  "Troisième dialogue"
+];
+
+// Récupération des éléments du DOM
+window.onload = function() {
+    
+  var texteDialogue = document.querySelector("#texte-dialogue");
+  var boutonPrecedent = document.querySelector("#bouton-precedent");
+  var boutonSuivant = document.querySelector("#bouton-suivant");
+  let isDisplayingText = false;
+
   // Initialisation des variables
   var indexDialogue = 0;
-  
+
+  function afficherLettre(texte) {
+    if(isDisplayingText){
+      return;
+    }
+    isDisplayingText = true;
+    let index = 0;
+    texteDialogue.textContent = "";
+    const intervalId = setInterval(() => {
+      texteDialogue.textContent += texte[index];
+      index++;
+      if (index === texte.length) {
+        clearInterval(intervalId);
+        isDisplayingText = false;
+      }
+    }, 50);
+  }
   // Fonction pour afficher le dialogue suivant
   function afficherDialogueSuivant() {
-    indexDialogue++;
-    texteDialogue.innerHTML = dialogues[indexDialogue];
-    boutonPrecedent.disabled = false;
-    if (indexDialogue === dialogues.length - 1) {
+    indexDialogue = dialogues.indexOf(texteDialogue.textContent)+1;
+    var nouveauDialogue = dialogues[indexDialogue];
+    afficherLettre(nouveauDialogue);
+    if(indexDialogue == dialogues.length-1){
       boutonSuivant.disabled = true;
     }
+    boutonPrecedent.disabled = false;
   }
-  
+
   // Fonction pour afficher le dialogue précédent
   function afficherDialoguePrecedent() {
-    indexDialogue--;
-    texteDialogue.innerHTML = dialogues[indexDialogue];
-    boutonSuivant.disabled = false;
-    if (indexDialogue === 0) {
-      boutonPrecedent.disabled = true;  
+    indexDialogue = dialogues.indexOf(texteDialogue.textContent)-1;
+    var nouveauDialogue = dialogues[indexDialogue];
+    afficherLettre(nouveauDialogue);
+    if(indexDialogue == 0){
+      boutonPrecedent.disabled = true;
     }
+    boutonSuivant.disabled = false;
+    
+
+
+    
   }
 
-
   
+
+  // ajout d'un eventlistenter sur le bouton suivant
+  boutonSuivant.addEventListener("click", afficherDialogueSuivant);
+  // pareil pour le bouton précédent
+  boutonPrecedent.addEventListener("click", afficherDialoguePrecedent);
+}
+
