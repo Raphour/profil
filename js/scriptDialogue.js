cb9ef66b8a77639ad21ab51445f1e24549fecf70
Raphour <leraphlol@gmail.com>
2023-04-29 18:18:42 +0200

    Fix JS + début CSS

--- js/scriptDialogue.js
+++ js/scriptDialogue.js
@@ -1,78 +1,80 @@
-// Tableau contenant les dialogues
-var dialogues = [
-  "Premier dialogue",
-  "Deuxième dialogue",
-  "Troisième dialogue"
-];
 
-// Récupération des éléments du DOM
-window.onload = function() {
-    
-  var texteDialogue = document.querySelector("#texte-dialogue");
-  var boutonPrecedent = document.querySelector("#bouton-precedent");
-  var boutonSuivant = document.querySelector("#bouton-suivant");
-  let isDisplayingText = false;
-
-  // Initialisation des variables
-  var indexDialogue = 0;
-
-  function afficherLettre(texte) {
-    if(isDisplayingText){
-      return;
-    }
-    isDisplayingText = true;
-    let index = 0;
-    texteDialogue.textContent = "";
-    const intervalId = setInterval(() => {
-      texteDialogue.textContent += texte[index];
-      index++;
-      if (index === texte.length) {
-        clearInterval(intervalId);
-        isDisplayingText = false;
-      }
-    }, 50);
-  }
-  // Fonction pour afficher le dialogue suivant
-  function afficherDialogueSuivant() {
-    indexDialogue = dialogues.indexOf(texteDialogue.textContent)+1;
-    var nouveauDialogue = dialogues[indexDialogue];
-    afficherLettre(nouveauDialogue);
-    if(indexDialogue == dialogues.length-1){
-      boutonSuivant.disabled = true;
-    }
-    boutonPrecedent.disabled = false;
-  }
-
-  // Fonction pour afficher le dialogue précédent
-  function afficherDialoguePrecedent() {
-    indexDialogue = dialogues.indexOf(texteDialogue.textContent)-1;
-    var nouveauDialogue = dialogues[indexDialogue];
-    afficherLettre(nouveauDialogue);
-    if(indexDialogue == 0){
-      boutonPrecedent.disabled = true;
-    }
-    boutonSuivant.disabled = false;
-    
+window.onload = function () {
+	var boutonPrecedent = document.querySelector("#bouton-precedent");
+	var boutonSuivant = document.querySelector("#bouton-suivant");
+	var texteDialogue = document.querySelector("#texte-dialogue");
+	var enTrainDEcrire = false;
+	var indexDialogue = 0;
+	
 
 
-    
-  }
 
-  
+	var dialogues = [
+		"Bonjour, je suis le professeur Chen.",
+		"Tu es un jeune dresseur de Pokémon, n'est-ce pas ?",
+		"Je suis heureux de te rencontrer.",
+		"Bienvenue dans le monde des Pokémon !",
+		
+	];
 
-  // ajout d'un eventlistenter sur le bouton suivant
-  boutonSuivant.addEventListener("click", afficherDialogueSuivant);
-  // pareil pour le bouton précédent
-  boutonPrecedent.addEventListener("click", afficherDialoguePrecedent);
+	// Fonction pour afficher un dialogue progressivement
+	function afficherLettre(texte) {
+		enTrainDEcrire = true; // Indique que l'animation est en cours
+		let index = 0;
+		const intervalId = setInterval(() => {
+		  texteDialogue	.textContent += texte[index];
+		  index++;
+		  if (index === texte.length) {
+			clearInterval(intervalId);
+			enTrainDEcrire = false; // Indique que l'animation est terminée
+		  }
+		}, 25);
+	  }
 
-  document.addEventListener("keydown", function(event) {
-    if (event.key === "a") {
-      afficherDialogueSuivant();
-    } else if (event.key === "b") {
-      afficherDialoguePrecedent();
-    }
-  });
+	afficherLettre(dialogues[indexDialogue]);
+
+	// Fonction pour afficher le dialogue suivant
+	function afficherDialogueSuivant() {
+		if(enTrainDEcrire) return; // Si l'animation est en cours, on ne fait rien
+		if (indexDialogue < dialogues.length - 1) {
+			indexDialogue = dialogues.indexOf(texteDialogue.textContent) + 1;
+			var nouveauDialogue = dialogues[indexDialogue];
+			texteDialogue.textContent = "";
+			afficherLettre(nouveauDialogue);
+			boutonPrecedent.disabled = false;
+			if (indexDialogue === dialogues.length - 1) {
+				boutonSuivant.disabled = true;
+			}
+		}
+	}
+
+	// Fonction pour afficher le dialogue précédent
+	function afficherDialoguePrecedent() {
+		if(enTrainDEcrire) return; // Si l'animation est en cours, on ne fait rien
+		if (indexDialogue > 0) {
+			indexDialogue = dialogues.indexOf(texteDialogue.textContent) - 1;
+			var nouveauDialogue = dialogues[indexDialogue];
+			texteDialogue.textContent = "";
+			afficherLettre(nouveauDialogue);
+			boutonSuivant.disabled = false;
+			if (indexDialogue === 0) {
+				boutonPrecedent.disabled = true;
+			}
+		}
+	}
+
+	// Ecouteur d'événement pour le bouton suivant
+	boutonSuivant.addEventListener("click", afficherDialogueSuivant);
+
+	// Ecouteur d'événement pour le bouton précédent
+	boutonPrecedent.addEventListener("click", afficherDialoguePrecedent);
+
+	// Ecouteur d'événement pour les touches du clavier
+	document.addEventListener("keydown", function (event) {
+		if (event.key === "b") {
+			afficherDialoguePrecedent();
+		} else if (event.key === "a") {
+			afficherDialogueSuivant();
+		}
+	});
 }
-
-
-
