f6828697913a56e3ad2b02f4dadc60427b18f79c
Raphour <leraphlol@gmail.com>
2023-04-29 22:49:18 +0200

    fix menu issues

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ function changeImage(element) {
     var image = element.querySelector("img");
     var nouvelleImage = "../assets/menu_button_hover.png";
     image.src = nouvelleImage;
-    element.style.width = "105%";
 }
 
 
@@ -11,5 +10,4 @@ function resetImage(element) {
     var image = element.querySelector("img");
     var nouvelleImage = "../assets/menu_button.png";
     image.src = nouvelleImage;
-    element.style.width = "100%";
 }
