83d3c0ec2ba39e5b6215db702f293ea9740ea8cd
Raphour <leraphlol@gmail.com>
2023-05-14 11:11:51 +0200

    creation page passion

---
+++ js/scriptDialoguePresentation.js
@@ -0,0 +1,80 @@
+
+window.onload = function () {
+	var boutonPrecedent = document.querySelector("#bouton-precedent");
+	var boutonSuivant = document.querySelector("#bouton-suivant");
+	var texteDialogue = document.querySelector("#texte-dialogue");
+	var enTrainDEcrire = false;
+	var indexDialogue = 0;
+	
+
+
+
+	var dialogues = [
+		"Bonjour, je me présente je m'appelle Raphaël Silly, j'ai 18 ans...",
+		"Je viens de Tullins une ville près de Grenoble et cette année j'ai déménagé à Nantes pour mes études d'informatique... ",
+		"Ce site va vous permettre d'en savoir plus sur moi, ma passion et pourquoi j'ai décidé d'en faire mon domaine d'études...",
+		"Si vous avez une question, une remarques ou des pistes d'améliorations, la rubrique Contact est la pour ça",
+		
+	];
+
+	// Fonction pour afficher un dialogue progressivement
+	function afficherLettre(texte) {
+		enTrainDEcrire = true; // Indique que l'animation est en cours
+		let index = 0;
+		const intervalId = setInterval(() => {
+		  texteDialogue	.textContent += texte[index];
+		  index++;
+		  if (index === texte.length) {
+			clearInterval(intervalId);
+			enTrainDEcrire = false; // Indique que l'animation est terminée
+		  }
+		}, 25);
+	  }
+
+	afficherLettre(dialogues[indexDialogue]);
+
+	// Fonction pour afficher le dialogue suivant
+	function afficherDialogueSuivant() {
+		if(enTrainDEcrire) return; // Si l'animation est en cours, on ne fait rien
+		if (indexDialogue < dialogues.length - 1) {
+			indexDialogue = dialogues.indexOf(texteDialogue.textContent) + 1;
+			var nouveauDialogue = dialogues[indexDialogue];
+			texteDialogue.textContent = "";
+			afficherLettre(nouveauDialogue);
+			boutonPrecedent.disabled = false;
+			if (indexDialogue === dialogues.length - 1) {
+				boutonSuivant.disabled = true;
+			}
+		}
+	}
+
+	// Fonction pour afficher le dialogue précédent
+	function afficherDialoguePrecedent() {
+		if(enTrainDEcrire) return; // Si l'animation est en cours, on ne fait rien
+		if (indexDialogue > 0) {
+			indexDialogue = dialogues.indexOf(texteDialogue.textContent) - 1;
+			var nouveauDialogue = dialogues[indexDialogue];
+			texteDialogue.textContent = "";
+			afficherLettre(nouveauDialogue);
+			boutonSuivant.disabled = false;
+			if (indexDialogue === 0) {
+				boutonPrecedent.disabled = true;
+			}
+		}
+	}
+
+	// Ecouteur d'événement pour le bouton suivant
+	boutonSuivant.addEventListener("click", afficherDialogueSuivant);
+
+	// Ecouteur d'événement pour le bouton précédent
+	boutonPrecedent.addEventListener("click", afficherDialoguePrecedent);
+
+	// Ecouteur d'événement pour les touches du clavier
+	document.addEventListener("keydown", function (event) {
+		if (event.key === "b") {
+			afficherDialoguePrecedent();
+		} else if (event.key === "a") {
+			afficherDialogueSuivant();
+		}
+	});
+}
